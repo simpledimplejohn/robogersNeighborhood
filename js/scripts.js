f3d1fa9b9ed3c40c28f79aac7af4bbf20e0ead3d
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-11 14:55:31 -0700

    first test passed

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,19 @@ $(document).ready(function() {
 
   let num1 = "1";
   
-
+// test number 1
   $("#number-form").submit(function(event) {
     event.preventDefault();
-    
-    let input = $('#number').val();
-    let inputArr = input.split(" ");
-    
-    function robogerize(array) {
-      
-      for(let i = 0; i < array.length; i++) {
-        let lookForOne = array[i];
-        
-        if(num1.indexOf(lookForOne) !== -1) {
-          array[i] =  "beep";
-        } 
-      }
-      return array;
+    let lowEnd = 0;
+    let highEnd = $('#number').val();
+    var array = [];
+    while(lowEnd <= highEnd) {
+      array.push(lowEnd++);
     }
-  
-    let robogerized = robogerize(inputArr).join(" ");
-  
-    $("#original").text(input);
-    $("#robogerized").text(robogerized);
-  })
+    
+    
+
+    $("#original").text(highEnd);
+    $("#robogerized").text(array);
   });
+});
