cf8a4457b5a3b9d5882d42f67786f011f5d5f199
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-11 15:34:30 -0700

    add neighbor test to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,17 @@ $(document).ready(function() {
       while(lowEnd <= highEnd) {
         array.push(lowEnd++);
       }
+      array.forEach(function(element) {
+        if (element === "3") {
+          array.push("Won't you be my neighbor");
+        }
+      });
       return array;
     }
+    
+
+
+
     let robogerized = robogerize(array)
 
     $("#original").text(highEnd);
