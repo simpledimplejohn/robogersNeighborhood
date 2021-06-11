338a525b028c3d03233d464d8cba948a9d64883c
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-11 12:25:52 -0700

    update scripts.js branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
 
-  $("#enterNumber-form").submit(function(event) {
+  let comparator = ['1', '2', '3', '4', '5'];
+  
+
+  $("#number-form").submit(function(event) {
     event.preventDefault();
+    
+    let input = $('#number').val();
+    let inputArr = input.split(" ");
+    
+    function robogerize(array) {
+      
+      
+      for(let i = 0; i < array.length; i++) {
+        let firstLetter = array[i].charAt(0);
+        
+        if(comparator.indexOf(firstLetter.toUpperCase()) !== -1) {
+          array[i] = array[i] + "ay";
+        } 
+        
 
-    const vowelArray = ["a", "e", "i", "o", "u"];
-    const text = $("numbers")
-
-    function wayAdder(text, vowelArray) {
-      for (let i = 0; i < vowelArray.length; i++) {
-        if (text.charAt(0) == vowelArray[i]) {
-          return (text + "way");
-        } else {
-          return false;
-        }
       }
+      return array;
     }
-    let output = wayAdder(inputArr).join(" ");
+  
+    let robogerized = robogerize(inputArr).join(" ");
+  
+    $("#original").text(input);
+    $("#robogerized").text(robogerized);
+  })
   });
-});
-
